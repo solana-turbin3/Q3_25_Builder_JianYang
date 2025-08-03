e1b8e6ed4630651d83f4032d099895b52962432e
elec7rik <vickyvirus258@gmail.com>
2025-08-04 02:36:24 +0530

    Add more handlers and comments to simple-nft-staking

--- simple-nft-staking/programs/simple-nft-staking/src/lib.rs
+++ simple-nft-staking/programs/simple-nft-staking/src/lib.rs
@@ -12,11 +12,61 @@ pub mod simple_nft_staking {
 
     use super::*;
 
+    pub fn initialize_config(
+        ctx: Context<InitializeConfig>,
+        points_per_second: u64,
+        collection_mint: Pubkey,
+        max_stake_per_user: u8,
+        freezing_period: i64,
+    ) -> Result<()> {
+        let stake_config = &mut ctx.accounts.stake_config;
+        stake_config.authority = ctx.accounts.authority.key();
+        stake_config.collection_mint = collection_mint;
+        stake_config.points_per_second = points_per_second;
+        stake_config.max_stake_per_user = max_stake_per_user;
+        stake_config.freeze_period = freezing_period;
+        stake_config.bump = ctx.bumps.stake_config;
+        Ok(())
+    }
+
+    pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.staker = ctx.accounts.payer.key();
+        user_account.accumulated_points = 0;
+        user_account.last_claim = Clock::get()?.unix_timestamp;
+        user_account.total_staked = 0;
+        user_account.bump = ctx.bumps.user_account;
+        Ok(())
+    }
     pub fn stake(ctx: Context<Stake>, nft_mint: Pubkey) -> Result<()> {
+        let stake_config = &mut ctx.accounts.stake_config;
         let stake_account = &mut ctx.accounts.stake_account;
         stake_account.staker = ctx.accounts.payer.key();
         stake_account.staked_at = Clock::get()?.unix_timestamp;
         stake_account.nft_mint = nft_mint;
+        stake_account.last_updated = Clock::get()?.unix_timestamp;
+        stake_account.staker_stats = ctx.accounts.user_account.key();
+        
+        let user_account = &mut ctx.accounts.user_account;
+        // CHECK:: User staking limit has not been crossed
+        require!(
+            user_account.total_staked < stake_config.max_stake_per_user,
+            ErrorCode::MaxStakeExceeded
+        );
+
+        // CHECK:: User owns the NFT
+        require!(
+            ctx.accounts.user_nft_account.owner == ctx.accounts.payer.key(),
+            ErrorCode::NotNFTOwner
+        );
+
+        // CHECK:: User is staking only one NFT per instruction
+        require!(
+            ctx.accounts.user_nft_account.amount == 1,
+            ErrorCode::InvalidNFTAmount
+        );
+        // user stats update
+        user_account.total_staked += 1;
 
         let cpi_accounts = token::Transfer {
             from: ctx.accounts.user_nft_account.to_account_info(),
@@ -28,11 +78,16 @@ pub mod simple_nft_staking {
         token::transfer(cpi_ctx, 1)?;
         Ok(())
     }
-
+    
     pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
         let stake_account = &mut ctx.accounts.stake_account;
         require!(stake_account.staker == ctx.accounts.payer.key(), ErrorCode::UnauthorizedUnstake);
         require!(stake_account.staked_at > 0, ErrorCode::NotStaked);
+        
+        // user stats update
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.total_staked -= 1;
+
         let cpi_accounts = token::Transfer {
             from: ctx.accounts.nft_vault.to_account_info(),
             to: ctx.accounts.user_nft_account.to_account_info(),
@@ -54,6 +109,35 @@ pub mod simple_nft_staking {
     }
 }
 
+#[derive(Accounts)]
+pub struct InitializeConfig<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + StakeConfig::INIT_SPACE,
+        seeds = [b"stake_config"],
+        bump
+    )]
+    pub stake_config: Account<'info, StakeConfig>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeUser<'info> {
+    #[account(mut)]
+    payer: Signer<'info>,
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + UserAccount::INIT_SPACE,
+        seeds = [b"user_account", payer.key.as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    pub system_program: Program<'info, System>,
+}
 #[derive(Accounts)]
 #[instruction(nft_mint: Pubkey)]
 pub struct Stake<'info> {
@@ -85,6 +169,16 @@ pub struct Stake<'info> {
         bump
     )]
     pub vault_authority: UncheckedAccount<'info>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        space = 8 + UserAccount::INIT_SPACE,
+        seeds = [b"user_account", payer.key().as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    #[account(seeds = [b"stake_config"], bump = stake_config.bump)]
+    pub stake_config: Account<'info, StakeConfig>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
 }
@@ -93,6 +187,7 @@ pub struct Stake<'info> {
 pub struct Unstake<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+    pub nft_mint: Account<'info, Mint>,
     #[account(
         mut,
         seeds = [b"stake_account", nft_mint.key().as_ref(), payer.key().as_ref()],
@@ -107,7 +202,14 @@ pub struct Unstake<'info> {
         bump
     )]
     pub vault_authority: UncheckedAccount<'info>,
-    pub nft_mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        space = 8 + UserAccount::INIT_SPACE,
+        seeds = [b"user_account", payer.key().as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
 }
@@ -120,4 +222,12 @@ pub enum ErrorCode {
     NotStaked,
     #[msg("Invalid stake account")]
     InvalidStakeAccount,
-}
+    #[msg("Mismatched NFT Mint")]
+    InvalidCollection,
+    #[msg("Staker's Max Stake Amount Exceeded")]
+    MaxStakeExceeded,
+    #[msg("Invalid NFT Amount")]
+    InvalidNFTAmount,
+    #[msg("Staker Is Not NFT Owner")]
+    NotNFTOwner,
+} 
--- simple-nft-staking/programs/simple-nft-staking/src/state.rs
+++ simple-nft-staking/programs/simple-nft-staking/src/state.rs
@@ -1,14 +1,37 @@
 use anchor_lang::prelude::*;
 
+// StakeConfig is the global config for a particular staking program
+// each deployment of the program initializes a global config using StakeConfig
 #[account]
-pub struct GlobalConfig {
-    pub authority: Pubkey,
+#[derive(InitSpace)]
+pub struct StakeConfig {
+    pub authority: Pubkey,          // program admin - authoritative control
+    pub collection_mint: Pubkey,
+    pub points_per_second: u64,
+    pub max_stake_per_user: u8,     // max no. of NTFs a single user can stake
+    pub freeze_period: i64,         // minimum time before the user can unstake
+    pub bump: u8,
 }
 
+// UserAccount is created once per user
+// it takes care of the metrics of each user
+#[account]
+#[derive(InitSpace)]
+pub struct UserAccount {
+    pub staker: Pubkey,
+    pub accumulated_points: u64,
+    pub total_staked: u8,
+    pub last_claim: i64,
+    pub bump: u8,
+}
+
+// StakeAccount is created once per user per nft
 #[account]
 #[derive(InitSpace)]
 pub struct StakeAccount {
     pub staker: Pubkey,
-    pub staked_at: i64,
     pub nft_mint: Pubkey,
+    pub staked_at: i64,
+    pub last_updated: i64,
+    pub staker_stats: Pubkey,
 }
